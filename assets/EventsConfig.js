0a369fc8c33a3436ba65190f43dd14422de33f16
Egor Nazarov <nazarov.egora@gmail.com>
2021-01-31 03:41:22 +0500

    Обновлены диалоги из сценария

---
+++ assets/EventsConfig.js
@@ -0,0 +1,14 @@
+import {Config} from './config';
+
+export const Events = {
+    event1: 10000,
+    event2: 20000,
+    event3: 30000,
+    event4: 40000,
+    event5: 50000,
+    event6: 50000,
+    oxygen1: 60000,
+    oxygen2: 70000,
+    oxygen3: 80000,
+    oxygenMushroom: 90000,
+};
