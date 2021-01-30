3878d78a9bd7ced105af9ec2412f9eb1153c6cc3
Danil 'Addeu' Deulin <addeulin@gmail.com>
2021-01-30 17:52:34 +0500

    Запуск игровых сцен из меню

--- assets/scripts/scenes/MenuScene.js
+++ assets/scripts/scenes/MenuScene.js
@@ -4,8 +4,18 @@ const {ccclass} = cc._decorator;
 
 @ccclass
 export default class MenuScene extends BaseScene {
+
 	onLoad() {
 		super.onLoad();
 	}
 
+	startNewGame() {
+		cc.director.loadScene('Intro');
+		cc.log('Intro Launched');
+	}
+
+	resumeGame() {
+		cc.director.loadScene('Main');
+		cc.log('Main Scene Launched');
+	}
 }
