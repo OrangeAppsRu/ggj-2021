4c90a864fddd54be2a4bf75e17a2e33fd5c208dd
Danil 'Addeu' Deulin <addeulin@gmail.com>
2021-01-31 02:20:24 +0500

    Изменение направления спрайта после движения

--- assets/scripts/game/Hero.js
+++ assets/scripts/game/Hero.js
@@ -12,7 +12,17 @@ export class Hero extends cc.Component {
         return this.node.parent.convertToWorldSpaceAR(this.node.position.add(this._anchor.position));
     }
 
-    playMoveAnimation() {
+    playMoveAnimation(direction) {
+        const {x, y} = {x: direction.x, y: direction.y};
+
+        if (x === 1 && y === 0) {
+            this.node.setScale(1);
+        }
+
+        if (x === 0 && y === 1) {
+            this.node.setScale(-1, 1);
+        }
+
         this.getComponent(cc.Animation).play('DudeFront');
     }
 }
