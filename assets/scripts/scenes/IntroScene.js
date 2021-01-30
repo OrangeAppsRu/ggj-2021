b3d4ca2814f3c17f16db30306c638153ad29f7d9
Egor Nazarov <nazarov.egora@gmail.com>
2021-01-31 00:33:52 +0500

    Заготовка для вызова окна и рендер текста как в интро

--- assets/scripts/scenes/IntroScene.js
+++ assets/scripts/scenes/IntroScene.js
@@ -1,10 +1,13 @@
 import BaseScene from './BaseScene';
 import {Locale} from '../Locale';
 
-const {ccclass} = cc._decorator;
+const {ccclass, property} = cc._decorator;
 
 @ccclass
 export default class IntroScene extends BaseScene {
+	@property(cc.Prefab)
+	window = null;
+
 	onLoad () {
 		super.onLoad();
 
@@ -14,12 +17,26 @@ export default class IntroScene extends BaseScene {
 		this.processDialogue();
 
 		this.node.on(cc.Node.EventType.TOUCH_START, this.processDialogue, this);
+
+		// this.openWindow();
+	}
+
+	openWindow () {
+		if (this.window) {
+			const window = cc.instantiate(this.window);
+
+			this.node.addChild(window);
+			this.renderText(window.getChildByName('description'));
+		}
 	}
 
 	processDialogue () {
-		const dialogueNode = this.node.getChildByName('dialogue');
-		const dialogueImageComponent = dialogueNode.getComponent('DialogueImage');
-		const typableTextComponent = dialogueNode.getComponent('TypableText');
+		this.renderText(this.node.getChildByName('dialogue'));
+	}
+
+	renderText (node) {
+		const dialogueImageComponent = node.getComponent('DialogueImage');
+		const typableTextComponent = node.getComponent('TypableText');
 
 		if (!typableTextComponent.isTextTyping) {
 			typableTextComponent.isTextTyping = true;
@@ -27,7 +44,9 @@ export default class IntroScene extends BaseScene {
 			const textToShow = this.dialogueText[this.playedText];
 
 			if (textToShow) {
-				dialogueImageComponent.setSpriteFrame(textToShow.character);
+				if (dialogueImageComponent) {
+					dialogueImageComponent.setSpriteFrame(textToShow.character);
+				}
 				typableTextComponent.processDialogue(textToShow);
 				this.playedText++;
 			}
