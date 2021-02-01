6251288ba515af75f9e1e1feb58258419d33b86f
Alexander Desinov <mrbz@ya.ru>
2021-02-01 13:28:24 +0500

    Добавлена сборка в docs

---
+++ docs/src/settings.6ea25.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Load.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"620a7",main:"a87f3"}};
