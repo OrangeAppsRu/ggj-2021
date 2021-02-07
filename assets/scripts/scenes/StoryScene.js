a81654005f1db5e588d8a79681839f416b2cf3d3
Danil 'Addeu' Deulin <addeulin@gmail.com>
2021-02-07 10:28:39 +0500

    Фоны загружаются как свойства сцены

--- assets/scripts/scenes/StoryScene.js
+++ assets/scripts/scenes/StoryScene.js
@@ -17,6 +17,22 @@ export default class StoryScene extends BaseScene {
     })
     _slideSound = null;
 
+    @property(cc.SpriteFrame)
+    intro1 = null;
+
+    @property(cc.SpriteFrame)
+    intro2 = null;
+
+    @property(cc.SpriteFrame)
+    intro3 = null;
+
+    @property(cc.SpriteFrame)
+    final = null;
+
+    @property(cc.SpriteFrame)
+    final2 = null;
+
+
     type = null;
     dialogueKey = '';
 
@@ -25,22 +41,25 @@ export default class StoryScene extends BaseScene {
     onLoad() {
         super.onLoad();
         StoryScene.instance = this;
-    }
-
-    start() {
         cc.audioEngine.stopAll();
         cc.audioEngine.playMusic(this._slideSound, true);
         this._dialogue = this.getComponent('Dialogue');
+    }
+
+    start() {
         this._dialogue.runTalk(this.dialogueKey);
         this._dialogue.node.on(Dialogue.EVENT_CHANGE_BG, this._updateBackground, this);
         this._dialogue.node.on(Dialogue.EVENT_DIALOGUE_END, this.onStoryEnd, this);
     }
+
     onDisable() {
         cc.audioEngine.stopAll();
+        this._dialogue.node.off(Dialogue.EVENT_CHANGE_BG, this._updateBackground, this);
+        this._dialogue.node.off(Dialogue.EVENT_DIALOGUE_END, this.onStoryEnd, this);
     }
 
     _updateBackground(bgName) {
-        let background = cc.assetManager.assets.find(ass => ass.name === bgName);
+        let background = this[bgName];
 
         if (background) {
             this._background.spriteFrame = background;
